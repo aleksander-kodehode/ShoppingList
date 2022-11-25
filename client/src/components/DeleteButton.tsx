1148fd788cd379d04564f3e32c4557c8f29fe1f3
Aleksander <aleksander.kodehode@gmail.com>
2022-11-25 14:29:47 +0100

    commented out deletebutton

--- client/src/components/DeleteButton.tsx
+++ client/src/components/DeleteButton.tsx
@@ -1,22 +1,22 @@
-import { ExclamationCircleFilled } from "@ant-design/icons";
-import { Button, Modal } from "antd";
+// import { ExclamationCircleFilled } from "@ant-design/icons";
+// import { Button, Modal } from "antd";
 
-const { confirm } = Modal;
-//TODO: COMEBACK TO THIS LATER
-const DeleteButton = () => {
-  const showPromiseConfirm = () => {
-    confirm({
-      title: "Are you sure you want to delete this item?",
-      icon: <ExclamationCircleFilled />,
-      content:
-        "When clicked the OK button, this dialog will be closed after 1 second",
-      onOk() {
-        handleItemDelete(item.itemId);
-      },
-      onCancel() {},
-    });
-  };
-  return <Button onClick={showPromiseConfirm}>Delete</Button>;
-};
+// const { confirm } = Modal;
+// //TODO: COMEBACK TO THIS LATER
+// const DeleteButton = () => {
+//   const showPromiseConfirm = () => {
+//     confirm({
+//       title: "Are you sure you want to delete this item?",
+//       icon: <ExclamationCircleFilled />,
+//       content:
+//         "When clicked the OK button, this dialog will be closed after 1 second",
+//       onOk() {
+//         handleItemDelete(item.itemId);
+//       },
+//       onCancel() {},
+//     });
+//   };
+//   return <Button onClick={showPromiseConfirm}>Delete</Button>;
+// };
 
-export default DeleteButton;
+// export default DeleteButton;
