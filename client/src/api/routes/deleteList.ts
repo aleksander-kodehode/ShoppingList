a6c505aee2810d518bde1ff31cf88c77537d6832
aleksander-kodehode <aleksander.kodehode@gmail.com>
2022-11-27 21:37:03 +0100

    type refactoring

--- client/src/api/routes/deleteList.ts
+++ client/src/api/routes/deleteList.ts
@@ -1,12 +1,14 @@
+import { authHeader } from "../../services/authHeader";
 import apiConfig from "../config";
 
 const deleteList = async (userId: string, listId: string) => {
+  const token = authHeader();
   const res = await fetch(`${apiConfig.server}/user/${userId}`, {
     method: "DELETE",
     body: JSON.stringify({
       listId: listId,
     }),
-    headers: { "Content-Type": "application/json" },
+    headers: { "Content-Type": "application/json", Authorization: token! },
   });
   return res.json();
 };
