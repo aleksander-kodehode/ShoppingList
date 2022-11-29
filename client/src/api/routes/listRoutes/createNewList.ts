c2acebe3bfc782eeac00f025099e0a41a04db8d8
Aleksander <aleksander.kodehode@gmail.com>
2022-11-29 14:36:44 +0100

    auth added to route

--- client/src/api/routes/listRoutes/createNewList.ts
+++ client/src/api/routes/listRoutes/createNewList.ts
@@ -1,12 +1,14 @@
+import { authHeader } from "../../../services/authHeader";
 import apiConfig from "../../config";
 
 const createList = async (userId: string, title: string) => {
+  const token = authHeader();
   const res = await fetch(`${apiConfig.server}/user/${userId}`, {
     method: "POST",
     body: JSON.stringify({
       title: title,
     }),
-    headers: { "Content-Type": "application/json" },
+    headers: { "Content-Type": "application/json", Authorization: token! },
   });
   return res.json();
 };
